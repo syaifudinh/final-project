31b43ffcd88906f3417cd6f681b22ff4594d7c61
syaifudinh <msyaifudinhidayat@gmail.com>
2025-05-18 20:51:05 +0700

    Login.cy.js

--- cypress/e2e/1-getting-started/Login.cy.js
+++ cypress/e2e/1-getting-started/Login.cy.js
@@ -3,24 +3,15 @@ import login from "../3-pageobejct/loginpage.js";
 describe('pom', () =>{
     it('login', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        cy.get('input[name="username"]').type('admin')
-        cy.get('input[name="password"]').type('admin123') 
-        cy.get('.oxd-button').click()
-        cy.get(".oxd-topbar-header-breadcrumb > .oxd-text").should('have.text','Dashboard');
-    });
-
-
-     it.only('LoginTest', () => {
-        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-
-     const ln=new login();
+      login.setUserName("admin")
+      login.setPassword("admin123")
+       login.clickSubmit();
+       login.verifyLogin();
         
-       ln.setUserName("admin")
-       ln.setPassword("admin123")
-       ln.clickSubmit();
-       ln.verifyLogin();
     });
+
 });
+
 
 //     export default PageObject
 
